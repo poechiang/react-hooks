20491bcdae35ba37831de0ba8ed1a97878709f58
poechiang <poechiang@live.cn>
2024-04-15 02:30:23 +0800

    feat: add useClassName

---
+++ packages/r-components/src/hooks/useClassName.ts
@@ -0,0 +1,12 @@
+export const useClassName = (props: {
+  className?: string;
+  classes?: Record<string, boolean>;
+}) => {
+  return Object.entries(props.classes || {})
+    .filter(([_, value]) => !!value)
+    .reduce(
+      (list, [key]) => [...list, key],
+      (props?.className || '').split(' '),
+    )
+    .join(' ');
+};
