96edf83ef0586e4fdc9ac47cd0976e030599ec05
poechiang <poechiang@live.cn>
2024-04-22 00:34:50 +0800

    fix: update fetchjs version

--- src/components/AuthProvider/index.tsx
+++ src/components/AuthProvider/index.tsx
@@ -1,11 +1,14 @@
+import { withFetcher } from '@jeffchi/fetchjs';
+import { withTags } from '@jeffchi/logger';
 import { createContext, FC, PropsWithChildren, useState } from 'react';
 import { useStorage } from '../../hooks';
 const KEY_LOGIN_USER_INFO = 'loginUserInfo';
 const KEY_LOGIN_USER_TOKEN = 'token';
-
+const { log } = withTags('AuthProvider');
+log(123);
 export const AuthContext = createContext<IAuthContext>(null!);
 
-export const AuthProvider: FC<PropsWithChildren> = ({ children }) => {
+export const AuthProvider: FC<PropsWithChildren> = ({ onSignin, children }) => {
   const { local, session } = useStorage();
   const u = session(KEY_LOGIN_USER_INFO) || null;
   const [user, setUser] = useState(u || null!);
@@ -13,8 +16,13 @@ export const AuthProvider: FC<PropsWithChildren> = ({ children }) => {
   const signin = async ({ token }: { token: string }) => {
     local(KEY_LOGIN_USER_TOKEN, token);
 
-    const loginUser = null;
-    setUser(loginUser!);
+    const { get } = withFetcher();
+    const res = await get('/auth/me', null, { enableResultPrompt: false }); // 获取当前登录用户信息;
+    if (res.code !== 0) {
+      return null!;
+    }
+    const loginUser = res.payload;
+    setUser(loginUser);
 
     session(KEY_LOGIN_USER_INFO, loginUser);
     return { user: loginUser };
